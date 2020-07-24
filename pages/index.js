a62fc18a5ef509a356c59251b400fd68a42ef3a4
abe <me@abevalle.com>
2020-07-24 17:00:27 -0500

    change footer

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>You are cake?</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -12,50 +12,23 @@ export default function Home() {
         <h1 className="title">
           Are You Cake?
         </h1>
-
         <p className="description">
           The test is coming soon...
         </p>
-
-        {/* <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div> */}
       </main>
 
       <footer>
+        <a 
+          href="https://mollysexwater.com"
+          >
+            Made by Mollysexwater </a>       
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Made with love by Mollysexwater {' '}
-          {/* <img src="/vercel.svg" alt="Vercel Logo" className="logo" /> */}
+          {' '} and Powered by {' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
 
